181283bdf0b46815d504647b7d1f4e9baa25196e
Lukas K Schumann <lukas_kilian.schumann@stud-mail.uni-wuerzburg.de>
2025-05-13 12:59:22 +0200

    docker images

--- tmux_session.sh
+++ tmux_session.sh
@@ -16,7 +16,7 @@ tmux set -st "$TMUX_SESSION_NAME" pane-border-status top
 tmux set -st "$TMUX_SESSION_NAME" pane-border-format "#T"
 
 
-tmux -u new-window -t "$TMUX_SESSION_NAME" -n ''
+tmux -u new-window -t "$TMUX_SESSION_NAME" -n '󱂚'
 tmux -u new-window -t "$TMUX_SESSION_NAME" -n '󱃖'
 tmux -u new-window -t "$TMUX_SESSION_NAME" -n '󰆼' "cd /home/lks/Documents/datastore/5g-masterarbeit/; zsh"
 
