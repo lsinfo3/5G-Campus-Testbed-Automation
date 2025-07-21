dede560fc218235708d0bcc050364d1dcb80a2b1
Lukas K Schumann <lukas_kilian.schumann@stud-mail.uni-wuerzburg.de>
2025-07-21 13:59:33 +0200

    updated docker files

--- docker/build_dockerfiles.sh
+++ docker/build_dockerfiles.sh
@@ -11,9 +11,15 @@ find . -maxdepth 1 -mindepth 1 -type d | while read d; do
     DOCKERIMAGE="$DOCKERUSER/$DOCKERDIR"
     echo "Building \"$DOCKERDIR\":"
 
-    time docker build --no-cache --tag "$DOCKERIMAGE" "$d" 1>"$DOCKERDIR"-build.log 2>&1
-    time docker push "$DOCKERIMAGE" 1>"$DOCKERDIR"-push.log 2>&1
+    if docker inspect "$DOCKERIMAGE" 1> /dev/null 2>&1; then
+        echo "Skipping $DOCKERIMAGE"
+    else
+        echo "Not skipping $DOCKERIMAGE"
+        time docker build --no-cache --tag "$DOCKERIMAGE" "$d" 1>"$DOCKERDIR"-build.log 2>&1
+        time docker push "$DOCKERIMAGE" 1>"$DOCKERDIR"-push.log 2>&1
+    fi
+    echo ""
+    echo ""
 
-    echo ""
-    echo ""
+
 done
